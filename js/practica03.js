62beb2d32db2684bdd703d132a064ed86aca4e33
aosuna08 <aosuna.dev@gmail.com>
2025-10-14 17:56:05 -0700

    Practica 03 finalizada

---
+++ js/practica03.js
@@ -0,0 +1,31 @@
+// Seleccionamos los objetos
+const valor = document.getElementById("valor");
+const btnIncrementar = document.getElementById("incrementar");
+const btnDisminuir = document.getElementById("disminuir");
+const btnReiniciar = document.getElementById("reiniciar");
+var contador = 0; // Inicializamos el contador
+
+// funciones
+function incrementar(){
+    contador++;
+    valor.textContent = contador; 
+}
+
+function decrementar(){
+    contador--;
+    valor.textContent = contador; 
+}
+
+function iniciar(){
+    contador=0;
+    valor.textContent = contador; 
+}
+
+// Evento para aumentar
+btnIncrementar.addEventListener("click",incrementar);
+
+// Evento para disminuir
+btnDisminuir.addEventListener("click", decrementar);
+
+// Evento para reiniciar
+btnReiniciar.addEventListener("click", iniciar);
